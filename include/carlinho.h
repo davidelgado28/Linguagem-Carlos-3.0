29acaf14120a673d8e276e041e45303711c17f20
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-12 19:57:17 -0300

    Update carlinho.h

--- include/carlinho.h
+++ include/carlinho.h
@@ -1,1 +1,16 @@
+#ifndef CARLINHO_H
+#define CARLINHO_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#define float static_assert(false, "O tipo 'float' não existe no Carlos. Use 'double'.");
+
+#define facilitador std
+
+#endif
